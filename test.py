c00d7abc0a5850e8fe235aaaf07f3722070c6701
OlzhasKALIEV <130698888+OlzhasKALIEV@users.noreply.github.com>
2023-07-03 23:17:33 +0600

    commit

--- test.py
+++ test.py
@@ -1,12 +1,42 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import os
 
-url = "https://ai-process-sandy.s3.eu-west-1.amazonaws.com/purge/deviation.json"
-df = pd.read_json(url)
-mean_deviation = df['mean'].mean()
-max_deviation = df['mean'].max()
-min_deviation = df['mean'].min()
-std_deviation = df['mean'].std()
-print(f"Среднее отклонение:, {mean_deviation}")
-print(f"Максимальное отклонение:, {max_deviation}")
-print(f"Минимальное отклонение:, {min_deviation}")
-print(f"Стандартное отклонение:, {std_deviation}")
+
+class PlotDrawer:
+    def __init__(self, data_folder):
+        self.data_folder = data_folder
+
+    def draw_plots(self, json_file):
+        # Чтение JSON-файла в DataFrame
+        df = pd.read_json(json_file)
+
+        # Создание папки для сохранения графиков
+        plots_folder = os.path.join(self.data_folder, "графики")
+        os.makedirs(plots_folder, exist_ok=True)
+
+        # Рисование графиков
+        paths = []
+        for column in df.columns:
+            plt.figure()
+            plt.plot(df[column])
+            plt.xlabel('Index')
+            plt.ylabel(column)
+            plt.title(f"График {column}")
+            file_name = f"{column}.png"
+            file_path = os.path.join(plots_folder, file_name)
+            plt.savefig(file_path)
+            paths.append(file_path)
+
+        return paths
+
+
+# Использование класса PlotDrawer и функции draw_plots
+json_file_url = "https://ai-process-sandy.s3.eu-west-1.amazonaws.com/purge/deviation.json"
+data_folder = "."
+plot_drawer = PlotDrawer(data_folder)
+paths = plot_drawer.draw_plots(json_file_url)
+# Вывод путей к графикам
+print("Пути к графикам:")
+for path in paths:
+    print(path)
